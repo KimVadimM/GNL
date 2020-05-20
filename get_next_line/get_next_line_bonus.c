7d79600d2d65da879629ea0b1ef9f2ff5fb56a6d
Вадим <vadim@MacBook-Pro-Vadim.local>
2020-05-21 03:03:52 +1000

    DYB

---
+++ get_next_line/get_next_line_bonus.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Smeeblin <kvm1986@yandex.ru>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/16 14:23:24 by Smeeblin          #+#    #+#             */
+/*   Updated: 2020/05/21 02:42:44 by Smeeblin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int		ft_checkline(char *back)
+{
+	int i;
+
+	i = 0;
+	while (back[i] != '\0')
+	{
+		if (back[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int		spit_line(char **back, int checkline, char **line, char **buffer)
+{
+	char *point;
+
+	(*back)[checkline] = '\0';
+	*line = ft_strdup(*back);
+	point = ft_strdup(*back + checkline + 1);
+	free(*back);
+	free(*buffer);
+	*back = point;
+	return (1);
+}
+
+int		return_all(char **backup, char **line, int read_size)
+{
+	if (read_size < 0)
+		return (-1);
+	else if (*backup)
+	{
+		*line = *backup;
+		*backup = 0;
+		return (0);
+	}
+	*line = ft_strdup("");
+	free(*backup);
+	return (0);
+}
+
+int		get_next_line(int fd, char **line)
+{
+	static char		*back[10];
+	ssize_t			sizeofread;
+	char			*buffer;
+	int				checkline;
+
+	if (fd < 0 || line == NULL)
+		return (-1);
+	if (BUFFER_SIZE <= 0)
+		return (-1);
+	if (!(buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (-1);
+	while ((sizeofread = read(fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		buffer[sizeofread] = '\0';
+		if (back[fd] == NULL)
+			back[fd] = ft_strdup("");
+		back[fd] = ft_strjoin(back[fd], buffer);
+		if ((checkline = ft_checkline(back[fd])) >= 0)
+			return (spit_line(&back[fd], checkline, line, &buffer));
+	}
+	free(buffer);
+	return (return_all(&back[fd], line, sizeofread));
+}
---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Smeeblin <kvm1986@yandex.ru>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/16 14:23:05 by Smeeblin          #+#    #+#             */
+/*   Updated: 2020/05/21 02:45:34 by Smeeblin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
+
+int		get_next_line(int fd, char **line);
+size_t	ft_strlen(const char *str);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strdup(char *src);
+#endif
